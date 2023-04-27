c8aba00e21d67e2d163bf66ca29491c7e430b7c2
likangbo <67063670@qq.com>
2023-04-27 22:32:22 +0800

    feat(排序): add 2-插入排序(优化：无监督的插入排序)

--- src/sort/insert-sort/index.spec.ts
+++ src/sort/insert-sort/index.spec.ts
@@ -1,7 +1,9 @@
 import { describe, it, expect } from 'vitest';
 import { cloneDeep, getExpendTime, getRandomArrCached } from '../../shared';
 import { checkOrderedSequence, SortRule } from '../index';
-import { insertSort } from './index';
+import { insertSort, insertSort2 } from './index';
+
+const mockArr = cloneDeep(getRandomArrCached(5000))
 
 describe('插入排序', () => {
   describe('升序', () => {
@@ -48,9 +50,59 @@ describe('插入排序', () => {
     const rule = SortRule.ASC
 
     it('n=5000', () => {
-      const mockArr = cloneDeep(getRandomArrCached(5000))
+      const { res: { arr }, expendTime } = getExpendTime(() => insertSort(cloneDeep(mockArr), rule))
+      // console.log(expendTime);
+      expect(checkOrderedSequence(arr, rule)).toBe(true)
+    })
+  })
+})
 
-      const { res: { arr }, expendTime } = getExpendTime(() => insertSort(mockArr, rule))
+describe('无监督的插入排序', () => {
+  describe('升序', () => {
+    const rule = SortRule.ASC
+
+    it('[2,1]', () => {
+      const { arr, count, exchangeCount } = insertSort2([2, 1], rule)
+
+      expect(arr).toEqual([1, 2])
+      expect(count).toBe(3)
+      expect(exchangeCount).toBe(1)
+    })
+
+    it('[2,1,5,7,4]', () => {
+      const { arr, count, exchangeCount } = insertSort2([2, 1, 5, 7, 4], rule)
+
+      expect(arr).toEqual([1, 2, 4, 5, 7])
+      expect(count).toBe(11)
+      expect(exchangeCount).toBe(7)
+    })
+  })
+
+  describe('降序', () => {
+    const rule = SortRule.DESC
+
+    it('[2,1]', () => {
+      const { arr, count, exchangeCount } = insertSort2([2, 1], rule)
+
+      expect(arr).toEqual([2, 1])
+      expect(count).toBe(1)
+      expect(exchangeCount).toBe(0)
+    })
+
+    it('[2,1,5,7,4]', () => {
+      const { arr, count, exchangeCount } = insertSort2([2, 1, 5, 7, 4], rule)
+
+      expect(arr).toEqual([7, 5, 4, 2, 1])
+      expect(count).toBe(11)
+      expect(exchangeCount).toBe(7)
+    })
+  })
+
+  describe('n个随机元素的数组，升序', () => {
+    const rule = SortRule.ASC
+
+    it('n=5000', () => {
+      const { res: { arr }, expendTime } = getExpendTime(() => insertSort2(cloneDeep(mockArr), rule))
       // console.log(expendTime);
       expect(checkOrderedSequence(arr, rule)).toBe(true)
     })
--- src/sort/insert-sort/index.ts
+++ src/sort/insert-sort/index.ts
@@ -29,3 +29,48 @@ export const insertSort = (arr: number[], rule: SortRule = SortRule.ASC): Return
     exchangeCount
   }
 };
+
+/**
+ * 无监督的插入排序
+ */
+export const insertSort2 = (arr: number[], rule: SortRule = SortRule.ASC): ReturnT => {
+  let count = 0
+  let exchangeCount = 0
+
+  const len = arr.length;
+
+  let index = 0
+  for (let i = 1; i < len; i++) {
+    if (checkQualified(arr[i], arr[i - 1], rule)) {
+      index = i
+      count++
+    }
+  }
+  while (index > 0) {
+    swap(arr, index, index - 1)
+    index--
+
+    count++
+    exchangeCount++
+  }
+
+  for (let i = 1; i < len; i++) {
+    let hit = 0
+
+    let j = i
+    while (checkQualified(arr[j], arr[j - 1], rule)) {
+      hit++
+      exchangeCount++
+      swap(arr, j, j - 1)
+      j--
+    }
+
+    count += hit || 1
+  }
+
+  return {
+    arr,
+    count,
+    exchangeCount
+  }
+};
